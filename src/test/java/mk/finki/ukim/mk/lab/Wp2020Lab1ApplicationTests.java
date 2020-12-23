bf1a4af5c8bc52aec85541c05670a0413f8546c0
Aleksandar Ivanovski <aleksandar.ivanovski123@gmail.com>
2020-12-23 13:55:10 +0100

    Add Integration Test
    Test /balloons from BalloonController
    Add properties for test profile

--- src/test/java/mk/finki/ukim/mk/lab/Wp2020Lab1ApplicationTests.java
+++ src/test/java/mk/finki/ukim/mk/lab/Wp2020Lab1ApplicationTests.java
@@ -1,13 +1,43 @@
 package mk.finki.ukim.mk.lab;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-@SpringBootTest
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class Wp2020Lab1ApplicationTests {
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup(WebApplicationContext wac) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testGetBalloonsPage() throws Exception {
+        MockHttpServletRequestBuilder balloonsRequest = MockMvcRequestBuilders.get("/balloons");
+
+        this.mockMvc.perform(balloonsRequest)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.model().attributeExists("balloons"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("allTypes"))
+                .andExpect(MockMvcResultMatchers.model().attribute("bodyContent", "list-balloons"))
+                .andExpect(MockMvcResultMatchers.view().name("master-template"));
     }
 
 }
