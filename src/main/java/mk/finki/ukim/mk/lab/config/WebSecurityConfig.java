1ce10cacbde186e9b3a7cadc7954878e3ab60d1b
Aleksandar Ivanovski <aleksandar.ivanovski123@gmail.com>
2021-01-14 10:33:22 +0100

    Add Remember Me functionality
    With DI UserService into WebSecurityConfig

--- src/main/java/mk/finki/ukim/mk/lab/config/WebSecurityConfig.java
+++ src/main/java/mk/finki/ukim/mk/lab/config/WebSecurityConfig.java
@@ -1,7 +1,7 @@
 package mk.finki.ukim.mk.lab.config;
 
+import mk.finki.ukim.mk.lab.service.UserService;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,19 +11,27 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserService userService;
+
+    public WebSecurityConfig(UserService userService) {
+        this.userService = userService;
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/home", "/assets/**", "/register","/balloons", "/api/**").permitAll()
+                .antMatchers("/", "/home", "/assets/**", "/register", "/balloons", "/api/**").permitAll()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
+
                 .and()
                 .formLogin()
                 .loginPage("/login").permitAll()
                 .failureUrl("/login?error=BadCredentials")
                 .defaultSuccessUrl("/balloons", true)
+
                 .and()
                 .logout()
                 .logoutUrl("/logout")
@@ -31,7 +39,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .invalidateHttpSession(true)
                 .deleteCookies("JSESSIONID")
                 .logoutSuccessUrl("/login")
+
                 .and()
-                .exceptionHandling().accessDeniedPage("/access_denied");
+                .exceptionHandling().accessDeniedPage("/access_denied")
+
+                .and()
+                .rememberMe().key("uniqueAndSecret")
+                .userDetailsService(userService);
     }
 }
